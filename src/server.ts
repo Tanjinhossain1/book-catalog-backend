ad8fb1a882b7ec10d9a4c49847289175eca2a654
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-09-02 01:01:27 +0600

    deloyeed

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,15 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+import app from './app';
+import config from './config/index';
+import { errorlogger, logger } from './shared/logger';
 
-const app = require('./app');
-const config = require('./config');
-const {errorlogger, logger} = require('./shared/logger');
-
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-
-  const server = app.listen(config.port, () => {
+  const server = app.listen(port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
-
     if (server) {
       server.close(() => {
         logger.info('Server closed');
