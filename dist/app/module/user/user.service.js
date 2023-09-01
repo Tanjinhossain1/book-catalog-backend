bf351d288d3974f4ddaf25d9d8e022ee30877919
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-09-01 23:27:30 +0600

    fix deploy problem

---
+++ dist/app/module/user/user.service.js
@@ -0,0 +1,49 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserService = void 0;
+const paginationHelper_1 = require("../../../helpers/paginationHelper");
+const prisma_1 = __importDefault(require("../../../shared/prisma"));
+const insertIntoDB = (data) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield prisma_1.default.user.create({
+        data
+    });
+    return result;
+});
+const getAllFromDB = (options) => __awaiter(void 0, void 0, void 0, function* () {
+    const { limit, page, skip } = paginationHelper_1.paginationHelpers.calculatePagination(options);
+    const result = yield prisma_1.default.user.findMany({
+        // where: whereConditions,
+        skip,
+        take: limit,
+        orderBy: options.sortBy && options.sortOrder
+            ? { [options.sortBy]: options.sortOrder }
+            : {
+                role: 'desc'
+            }
+    });
+    const total = yield prisma_1.default.user.count({});
+    return {
+        meta: {
+            total,
+            page,
+            limit
+        },
+        data: result
+    };
+});
+exports.UserService = {
+    insertIntoDB,
+    getAllFromDB,
+};
