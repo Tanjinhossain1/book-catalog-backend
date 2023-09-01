92461479e545a9108200b9409f170069835d0446
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-09-01 22:38:52 +0600

    make user Model

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import { UserRouter } from '../module/user/user.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
+    path: "/user",
+    routes: UserRouter
   }
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
